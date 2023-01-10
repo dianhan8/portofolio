106b1f986047da4da574ef06b8175c031d55b1b8
dianhan8 <dianhan8@gmail.com>
2023-01-10 16:26:41 +0700

    initial starter

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+const { fontFamily } = require('tailwindcss/defaultTheme')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#000000',
+        secondary: '#08090a',
+        vanila: '#7f7f7f',
+      },
+      fontFamily: {
+        sans: ['var(--font-inter)', ...fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+}
